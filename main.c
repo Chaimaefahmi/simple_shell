7d0e7b9071b1b6edc2a79b6f7a74da03ac1bca6d
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-12-16 02:44:40 +0000

    hello

--- main.c
+++ main.c
@@ -2,14 +2,28 @@
 /**
  * main - simple shell main function
  * @ac: count of arguments
- * @av: arguments
+ * @argv: arguments
  * Return: 0 always (success).
  */
 int main(int ac, char **argv)
 {
-	char *line = NULL;
-	char **command = NULL;
-       int status;
-(void) ac;
+	char *line = NULL, **command = NULL;
+	int status = 0;
+	(void) ac;
+	while (1)
+	{
+	line = read_line();
+	if (line == NULL /* reache EOF ctr + D */
+			{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "/n", 1);
+			return (status);
+			}
 
+	command = tokenizer(line);
+	if (!command)
+	continue;
 
+	statuts = _execute(command, argv);
+	}
+}
